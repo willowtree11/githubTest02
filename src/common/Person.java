a3cda145c9853c1d7de928365cef3722021db518
willowTree11 <tschichold05@gmail.com>
2020-11-06 18:01:29 +0900

    Conflict

--- src/common/Person.java
+++ src/common/Person.java
@@ -3,10 +3,11 @@ package common;
 public class Person {
 	String name;
 	int age;
+	String nickName;	
 	
 	public Person(String name, int age) {
 		super();
 		this.name = name;
 		this.age = age;
-	}	
+	}
 }
